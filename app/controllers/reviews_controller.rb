f3e8dcde88b9b57837ccc38f28327bff5b7a3c0f
Iryna Shymbor <i.shymbor@gmail.com>
2020-07-08 17:54:52 +1000

    added crud functionality to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,44 @@
 class ReviewsController < ApplicationController
+  before_action :find_review, only: [:show, :update, :destroy]
+
   def index
     reviews = Review.all
     render json: reviews
- end
+  end
+
+  def show 
+      render json: @review
+    end 
+
+    def create 
+      review = Review.new(review_params)
+      if(review.save)
+        render json: "added a new review", status: :created
+      else
+        render json: {error: review.errors.full_messages}, status: :unprocessable_entity
+      end
+    end 
+
+    def update 
+      if @review.update(review_params)
+        render status: :no_content
+      else
+        render json: {error: @review.errors.full_messages}, status: :unprocessable_entity
+      end
+    end 
+
+    def destroy
+      @review.delete
+      render status: :no_content
+    end 
+
+  private 
+
+  def review_params 
+    params.require(:review).permit(:title, :content, :country, :year)
+  end 
+
+  def find_review 
+    @review = Review.find(params[:id])
+  end 
 end
